263623254e6454f7bdafa18c723c187d0d4d9014
Anas Arkawi <deltamikemike12@gmail.com>
2022-08-20 22:15:02 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+/**
+ * Astro Buddy Discord Bot.
+ * By Anas Arkawi, 2022
+ */
+
+
+// Configure dotenv
+require('dotenv').config();
+
+
+// Import modules
+const discord = require('discord.js');
+
+// Import own modules
+const apiReqs = require('./apiReqs.js');
